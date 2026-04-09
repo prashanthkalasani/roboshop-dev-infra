69a21e25fd6512bd1efc93582d33883505830ba1
prashanthkalasani <prashanthkalasani143@gmail.com>
2026-04-09 18:04:56 +0530

    terraform-37

--- 00-VPC/main.tf
+++ 00-VPC/main.tf
@@ -1,18 +1,18 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    source = "git::https://github.com/daws-86s/terraform-aws-vpc.git?ref=main"
     # VPC
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
     vpc_tags = var.vpc_tags
 
-    #public subnets
+    # public subnets
     public_subnet_cidrs = var.public_subnet_cidrs
 
-    #private subnets
+    # private subnets
     private_subnet_cidrs = var.private_subnet_cidrs
 
-    #database subnets
+    # database subnets
     database_subnet_cidrs = var.database_subnet_cidrs
 
     is_peering_required = true
